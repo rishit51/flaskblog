8873d37a51cd477d8d5a8f2d5fa0b1d723280ebf
Rishit Srivastava <ui21cs51@iiitsurat.ac.in>
2023-06-20 02:12:44 +0530

    half baked blog

--- app.py
+++ app.py
@@ -45,3 +45,25 @@ def get_post(post_id):
 def post(post_id):
     post = get_post(post_id)
     return render_template('post.html', post=post)
+
+
+@app.route('/<int:id>/edit', methods=('GET', 'POST'))
+def edit(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE posts SET title = ?, content = ?'
+                         ' WHERE id = ?',
+                         (title, content, id))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('edit.html', post=post)
